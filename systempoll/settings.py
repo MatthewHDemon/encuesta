e0f576f003d062e139e8d18d6b3ada1fc15a71f6
MatthewHDemon <danielmateopatarroyomolina@gmail.com>
2022-03-11 21:39:56 -0500

    Final proyect

--- systempoll/settings.py
+++ systempoll/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from pickle import TRUE
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qwhdo58qi#vq4w7k4o8f=n6ctvhc1ys7xtbnb8&)r!9die)au3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = TRUE
 
 ALLOWED_HOSTS = []
 
